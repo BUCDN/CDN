23051d7674db6dc37c9b970c4d74132bbd7b12a9
BUCDN <76461682+BUCDN@users.noreply.github.com>
2021-05-08 17:07:12 +0800

    Add files via upload

---
+++ js/bdtj-bikeng.js
@@ -0,0 +1,8 @@
+/*ZHUYE.XYZ*/
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?532d23d8ec439852a1300eab92351fe0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
